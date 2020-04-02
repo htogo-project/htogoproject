d40b5ea4a76d4fdcd1abc6a5bf027e52e315ce53
Papatiara <shirazipatricia@gmail.com>
2020-04-01 18:35:35 -0700

    content type access allow

--- src/server.js
+++ src/server.js
@@ -8,10 +8,10 @@ const port = 8080;
 
 const app = express();
 
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 
 app.use((request, response, next) => {
