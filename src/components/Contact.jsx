75b225d4d8795cb4d2ba9252e9d5f76722fb118e
Papatiara <shirazipatricia@gmail.com>
2020-04-04 11:25:50 -0700

    Contact better

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,16 @@ import axios from 'axios';
 
 
 function Contact(props) {
+    const [sent, setSent] = useState("");
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [message, setMessage] = useState("");
+
+
+    const goBack = () => {
+        props.setAdc(false)
+    }
+
 
     const send = (event) => {
         event.preventDefault();
@@ -16,47 +23,56 @@ function Contact(props) {
             message: message
         };
 
-        axios.post("/api/v1", data, { 
+
+        axios.post("/api/v1", data, {
 
         })
-        .then( res => {
-            console.log("sent")
-            setName("");
-            setMessage("");
-            setEmail("");
-        })
-        .catch( (error) => {
-          console.log(error, 'Message not sent')
-        })
-        
+            .then(res => {
+                console.log("sent")
+                setName("");
+                setMessage("");
+                setEmail("");
+                setSent("Message sent! I will make the updated ASAP or get back to you for more info! =)")
+            })
+            .catch((error) => {
+                console.log(error, 'Message not sent')
+            })
+
     }
 
-     const handleChangeName = (event) => {
+    const emptySent = () => {
+        setSent("")
+    }
+
+    const handleChangeName = (event) => {
         setName(event.currentTarget.value)
-     }
-    
+    }
 
-     const handleChangeMessage = (event) => {
+
+    const handleChangeMessage = (event) => {
         setMessage(event.currentTarget.value)
-     }
+    }
 
-     const handleChangeEmail = (event) => {
+    const handleChangeEmail = (event) => {
         setEmail(event.currentTarget.value)
-     }
-    
-    
+    }
+
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
-                <form action="/action_page.php">
-                    <label > Your name</label>
-                    <input onChange={handleChangeName}className={styles.same} type="text" name="firstname" placeholder="Your Name"  value={name} />
-                    <label > E-mail</label>
-                    <input onChange={handleChangeEmail} className={styles.same} type="text" name="email" placeholder="Your Email"  value={email}/>
-                    <label > Message</label>
-                    <textarea onChange={handleChangeMessage} className={styles.same}  name="subject" placeholder="What would you like to add/remove or update?"  value={message}></textarea>
-                    <button onClick={send}className={styles.b} type="submit" value="Submit"> Send </button> 
-                </form>
+        <div>
+            <h4 className={styles.sent}>{sent}</h4>
+            <button onClick={goBack} className={styles.goBack}> Go Back </button>
+            <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <form action="/action_page.php">
+                        <label > Your name</label>
+                        <input onClick={emptySent} onChange={handleChangeName} className={styles.same} type="text" name="firstname" placeholder="Your Name" value={name} />
+                        <label > E-mail</label>
+                        <input onClick={emptySent}  onChange={handleChangeEmail} className={styles.same} type="text" name="email" placeholder="Your Email" value={email} />
+                        <label > Message</label>
+                        <textarea onClick={emptySent} onChange={handleChangeMessage} className={styles.same} name="subject" placeholder="What would you like to add/remove or update?" value={message}></textarea>
+                        <button onClick={send} className={styles.b} type="submit" value="Submit"> Send </button>
+                    </form>
+                </div>
             </div>
         </div>
     )
@@ -65,6 +81,7 @@ function Contact(props) {
 
 const styles = {
     wrapper: css`
+        height: 100vh;
         color: white;
         margin-top: 20px;
         display: flex;
@@ -95,6 +112,13 @@ const styles = {
             background-color: #45a049;
         }
     `,
+    sent:css`
+    color: black;
+    text-align: center;
+    `,
+    goBack: css`
+    margin-left: 20px;
+    `
 
 }
 
